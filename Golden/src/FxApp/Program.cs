a94eaa0a17acdf900629cc7930e41aea96fab94a
siyan <siyan121@outlook.com>
2017-03-08 11:41:41 +0800

    test

---
+++ Golden/src/FxApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FxApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"The answer is {new Thing().Get(42)}.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Golden/test/TestLibrary/LibraryTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using Library;
+namespace TestLibrary
+{
+    public class LibraryTests
+    {
+        [Fact]
+        public void ThingGetsObjectValFromNumber()
+        {
+            Assert.Equal(42, new Thing().Get(42));
+        }
+    }
+}
