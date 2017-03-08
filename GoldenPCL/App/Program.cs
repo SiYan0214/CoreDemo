d4dfd3d528ba6768092b850543e34fb1df6dff25
siyan <siyan121@outlook.com>
2017-03-08 14:48:49 +0800

    CoreMVC

---
+++ GoldenPCL/App/Program.cs
@@ -0,0 +1,18 @@
+﻿using PCLLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"The answer is {new Thing().Get(42)}.");
+            Console.ReadLine();
+        }
+    }
+}
